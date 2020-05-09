4b9ac97b94d20db3a5c366cb1057ed4a04943d9d
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-09 17:29:31 -0300

    subindo o projeto base, nele foram feitas as configurações iniciais, eslint, prettier e o estilo global inicial

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples
+  trailingComma: 'all', //no final dos objetos quero virgula
+  arrowParens: 'avoid', // para não colocar os parentense nas arrowfunction quando estiver apenas um parametro
+};
