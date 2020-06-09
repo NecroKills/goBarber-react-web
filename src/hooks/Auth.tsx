0c69640522356da97d7fa16115e0933e615430b7
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-08 21:05:17 -0300

    Foi adicionado uma interface no User que estava apenas como objeto. Começamos a fazer o header do Dashboard, criando os estilos e o index

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,20 +1,26 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 // Criando o contexto.
