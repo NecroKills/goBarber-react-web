39b8ec5e93e006e2cc91b5aceb94587fa43dccde
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-08 21:57:54 -0300

    Foi desenvolvido a listagem de manhã e de tarde, foi criado o Section, Appointment e seus estilos. Tambem foi adicionado um estado para controlar o dia que esta sendo mostrado na nossa listagem

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
@@ -10,12 +10,16 @@ import {
   Schedule,
   Calendar,
   NextAppointment,
+  Section,
+  Appointment,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <Container>
@@ -57,6 +61,39 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                02:00
+              </span>
+              <div>
+                <img
+                  src="https://api.adorable.io/avatars/80/abott@adorable.io.png"
+                  alt="oi"
+                />
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                16:00
+              </span>
+              <div>
+                <img
+                  src="https://api.adorable.io/avatars/80/abott@adorable.io.png"
+                  alt="oi"
+                />
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
