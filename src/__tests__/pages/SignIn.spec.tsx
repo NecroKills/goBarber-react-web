17ef06d2b60bd2dbc464fd2d305ea6946ec46ed0
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-21 12:30:20 -0300

    Foi convigurado o ambiente do jest, foi criado o primeiro teste do zero da pagina de SignIn, realizado o teste de login, foi feito o mock do hook de autenticação. Utilizamos o testing-library/react.

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('../../hooks/auth', () => {
+  return {
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+  });
+});
